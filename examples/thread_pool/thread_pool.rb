1d1cbc808a536a449b8f6dab5b9d4e0cb037f99c
Ross Kaffenberger <rosskaff@gmail.com>
2016-03-02 15:50:55 -0500

    ThreadPool 2: use threads for speed increase

--- examples/thread_pool/thread_pool.rb
+++ examples/thread_pool/thread_pool.rb
@@ -1,11 +1,13 @@
 class ThreadPool
   def initialize(size:)
+    @pool = []
   end
 
   def schedule(*args, &block)
-    block.call(args)
+    @pool << Thread.new { block.call(args) }
   end
 
   def shutdown
+    @pool.map(&:join)
   end
 end
--- examples/thread_pool/thread_pool_test.rb
+++ examples/thread_pool/thread_pool_test.rb
@@ -3,6 +3,12 @@ require 'minitest/pride'
 require_relative './thread_pool'
 
 class TestThreadPool < Minitest::Test
+  def time_taken
+    now = Time.now.to_f
+    yield
+    Time.now.to_f - now
+  end
+
   def test_basic_usage
     pool_size = 5
     pool = ThreadPool.new(size: pool_size)
@@ -22,5 +28,18 @@ class TestThreadPool < Minitest::Test
     pool.shutdown
 
     assert_equal((1.upto(pool_size * 3)).to_a, results)
+  end
+
+  def test_time_taken
+    pool_size = 5
+    pool = ThreadPool.new(size: pool_size)
+    elapsed = time_taken do
+      pool_size.times do
+        pool.schedule { sleep 1 }
+      end
+      pool.shutdown
+    end
+    assert_operator 4.5, :>, elapsed,
+      'Elased time was too long: %.1f seconds' % elapsed
   end
 end
