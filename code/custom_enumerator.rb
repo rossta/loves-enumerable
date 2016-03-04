55b626817c95b0d1554535a8f4fd6735d6730d97
Ross Kaffenberger <rosskaff@gmail.com>
2016-03-04 08:02:17 -0500

    Refactor CustomEnumerator

--- code/custom_enumerator.rb
+++ code/custom_enumerator.rb
@@ -1,22 +1,21 @@
+# source: practicingruby
+# see also: Fiber section of pickaxe book
 require "fiber"
 
 class CustomEnumerator
   def initialize(target, iter)
     @target = target
     @iter   = iter
+    rewind
   end
 
-  def each(&block)
-    @target.send(@iter, &block)
+  def each
+    return custom_enum(:each) unless block_given?
+
+    loop { yield self.next }
   end
 
   def next
-    @fiber ||= Fiber.new do
-      each { |e| Fiber.yield(e) }
-
-      raise StopIteration
-    end
-
     if @fiber.alive?
       @fiber.resume
     else
@@ -25,15 +24,35 @@ class CustomEnumerator
   end
 
   def rewind
-    @fiber = nil
+    @fiber = create_fiber
   end
 
   def with_index
+    return custom_enum(:with_index) unless block_given?
+
     i = 0
-    each do |e|
+    enumerate do |e|
       out = yield(e, i)
       i += 1
       out
     end
+  end
+
+  private
+
+  def create_fiber
+    Fiber.new do
+      enumerate { |e| Fiber.yield(e) }
+
+      raise StopIteration
+    end
+  end
+
+  def enumerate(&block)
+    @target.send(@iter, &block)
+  end
+
+  def custon_enum(method)
+    CustomEnumerator.new(@target, method)
   end
 end
