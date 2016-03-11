32016b3cec6a15a70c6e8aec45baac5c116bbb43
Ross Kaffenberger <rosskaff@gmail.com>
2016-03-11 16:26:46 -0500

    WIP parallel each

--- examples/parallel_each/parallel.rb
+++ examples/parallel_each/parallel.rb
@@ -3,29 +3,37 @@ require 'concurrent/configuration'
 require 'concurrent/executors'
 
 class Parallel
-  def initialize(enum)
+  def initialize(enum, opts = {})
     @enum = enum
+    @executor = Concurrent::Options.executor_from_options(opts) || Concurrent.global_io_executor
   end
 
-  def each(&block)
+  def each( &block)
     @enum.each(&block)
   end
 
   def map(&block)
-    size     = each.size
     results  = Array.new(size)
     latch    = Concurrent::CountDownLatch.new(size)
     index    = Concurrent::AtomicFixnum.new(-1)
-    executor = Concurrent::FixedThreadPool.new(size)
-    # executor = Concurrent.global_fast_executor
 
     each do |element|
       i = index.increment
-      executor.post { results[i] = yield(element); latch.count_down }
+      @executor.post { results[i] = yield(element); latch.count_down }
     end
 
     latch.wait
 
     results
+  end
+
+  def size
+    @enum.size
+  end
+end
+
+module Enumerable
+  def parallel(opts = {})
+    Parallel.new(self, opts)
   end
 end
--- examples/parallel_each/parallel_test.rb
+++ examples/parallel_each/parallel_test.rb
@@ -1,25 +1,24 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'benchmark'
 require_relative './parallel'
-
-module Enumerable
-  def parallel
-    Parallel.new(self)
-  end
-end
 
 class TestParallel < Minitest::Test
   def time_taken
-    t = Time.now.to_f
     yield
-    Time.now.to_f - t
   end
 
   def test_map_result
     assert_equal [2, 4, 6, 8], [1, 2, 3, 4].parallel.map { |x| x * 2  }
   end
 
+  def test_each_result
+    count = 0
+    [1, 2, 3, 4].parallel.each { |x| count += x  }
+    assert_equal 10, count
+  end
+
   def test_saves_time
-    assert_operator 3.5, :> , time_taken { [1, 2, 3, 4].parallel.map { sleep 1 } }
+    assert_operator 0.4, :> , time_taken { [1, 2, 3, 4].parallel.map { sleep 0.1 } }
   end
 end
